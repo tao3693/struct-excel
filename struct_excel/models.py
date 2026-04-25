b4c85745430b309e402fa84ba2589274129c50ac
Fovir <fovir@disroot.org>
2026-04-25 19:24:17 +0800

    feat(model): add `RawRow` model and `Sector` enum

--- struct_excel/models.py
+++ struct_excel/models.py
@@ -14,6 +14,12 @@ class SessionMode(Enum):
     OFFLINE = "offline"
 
 
+class Sector(Enum):
+    GOVERNMENT = "Government"
+    PRIVATE = "Private"
+    STUDENT = "Student"
+
+
 @dataclass
 class Student:
     student_id: int
@@ -23,7 +29,7 @@ class Student:
     it_background: bool
     experience_min_years: int
     experience_max_years: int
-    sector: str
+    sector: Sector
     supervisor_id: int
 
 
@@ -59,3 +65,24 @@ class Enrollment:
     completed: bool
     payment_status: str
     exception: str
+
+
+@dataclass
+class RawRow:
+    reg_date: datetime
+    student_full_name: str
+    student_email: str
+    student_company: str
+    student_job_title: str
+    country: str
+    exception: str
+    phone: str
+    course: str
+    gender: str
+    sector: str
+    supervisor_name: str
+    supervisor_email: str
+    it_background: str
+    experience: str
+    completed: str
+    payment_status: str
